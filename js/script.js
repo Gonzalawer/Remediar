a0e60cf600b5ea1989d4470fdd4cc46f5016af70
Gonzalawer <gonza.8bits@gmail.com>
2023-05-08 11:13:08 -0600

    Sección footer terminada

--- js/script.js
+++ js/script.js
@@ -29,3 +29,18 @@ const toTop = (() => {
         window.scrollTo({ top: 0, behavior: "smooth" })
     }
 })();
+
+/* desplazamiento suave links */
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        var destino = $(this.hash);
+        if (destino.length == 0) {
+            destino = $('a[name="' + this.hash.substr(1) + '"]');
+        }
+        if (destino.length == 0) {
+            destino = $('html');
+        }
+        $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+        return false;
+    });
+});
