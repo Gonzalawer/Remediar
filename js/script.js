362325add66439682839171075ad89960341aff7
Gonzalawer <gonza.8bits@gmail.com>
2023-05-11 10:44:19 -0600

    Version 0.4

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-const userNameField = document.querySelector("[name=username]");
-const MailField = document.querySelector("[name=email]");
-const BusinessField = document.querySelector("[name=business]");
-const MessageField = document.querySelector("[name=message]");
-
-console.log(userNameField, MailField, BusinessField, MessageField);
-
-// e: evento - target: constante (userNameField) value: el valor o contenido de la constante
-userNameField.addEventListener("blur", function (e) {
-    const field = e.target.value;
-    const fieldValue = e.target.value;
-    if (fieldValue.length === 0) {
-        field.parentElement.insertAdjacentHTML(
-            "beforeend",
-            "<span>Debes colocar un nombre</span>"
-        );
-    }
-})
-
 /* Botón Subir */
 const toTop = (() => {
     let button = document.getElementById("toTop");
@@ -30,17 +11,3 @@ const toTop = (() => {
     }
 })();
 
-/* desplazamiento suave links */
-$(document).ready(function () {
-    $('a[href^="#"]').click(function () {
-        var destino = $(this.hash);
-        if (destino.length == 0) {
-            destino = $('a[name="' + this.hash.substr(1) + '"]');
-        }
-        if (destino.length == 0) {
-            destino = $('html');
-        }
-        $('html, body').animate({ scrollTop: destino.offset().top }, 500);
-        return false;
-    });
-});
